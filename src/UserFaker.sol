19c402807237007e95e05446fe832788922a08d7
Brendan Asselstine <mail.asselstine@gmail.com>
2023-03-23 15:36:27 -0700

    Added withdraw code

--- src/UserFaker.sol
+++ src/UserFaker.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.18;
+pragma solidity 0.8.17;
 
-import { Create2 } from "openzeppelin/contracts-upgradeable/utils/Create2.sol";
+import { ERC4626 } from "openzeppelin/token/ERC20/extensions/ERC4626.sol";
+import { Create2 } from "openzeppelin/utils/Create2.sol";
 import { FakeUser, Call } from "./FakeUser.sol";
 import { MinimalProxyLibrary } from "./libraries/MinimalProxyLibrary.sol";
+import { Vault } from "v5-vault/Vault.sol";
+import { Address } from "openzeppelin/utils/Address.sol";
 
 contract UserFaker {
+  using Address for address;
 
   FakeUser fakeUserInstance;
   bytes internal fakeUserBytecode;
 
-  mapping(address => uint256) vaultFakeUserCount;
-
-  int lastCreatedFakeUser = -1;
-
+  mapping(Vault => uint256) vaultFakeUserCount;
+  
   /// @notice Constructs a new controller.
   /// @dev Creates a new FakeUser instance and an associated minimal proxy.
   constructor () public {
@@ -22,37 +24,43 @@ contract UserFaker {
     fakeUserBytecode = MinimalProxyLibrary.minimalProxy(address(fakeUserInstance));
   }
 
-  function setFakeUsers(address vault, uint256 count) public {
-    uint currentCount = vaultFakeUserCount[vault];
+  function setFakeUsers(Vault _vault, uint256 count) public {
+    uint currentCount = vaultFakeUserCount[_vault];
     if (count < currentCount) {
       for (uint i = count; i < currentCount; i++) {
-        _withdrawUser(vault, i);
+        _withdrawUser(_vault, i);
       }
     } else {
       for (uint i = currentCount; i < count; i++) {
-        _depositUser(vault, i);
+        _depositUser(_vault, i);
       }
     }
   }
 
-  function _withdrawUser(address _vault, uint256 _userIndex) internal {
+  function _withdrawUser(Vault _vault, uint256 _userIndex) internal {
     FakeUser fakeUser = FakeUser(computeAddress(_vault, _userIndex));
+    uint256 balance = _vault.balanceOf(address(fakeUser));
     Call[] memory calls = new Call[](1);
     calls[0] = Call({
-      to: _vault,
-      data:     
-    })
+      to: address(_vault),
+      value: 0,
+      data: abi.encodeWithSelector(ERC4626.redeem.selector, balance, address(fakeUser), address(fakeUser))
+    });
     fakeUser.executeCalls(calls);
   }
 
-  function _depositUser(address _vault, uint256 _userIndex) internal {
-    FakeUser fakeUser;
-    if (lastCreatedFakeUser < userIndex) {
+  function _depositUser(Vault _vault, uint256 _userIndex) internal {
+    FakeUser fakeUser = FakeUser(computeAddress(_vault, _userIndex));
+    if (!address(fakeUser).isContract()) {
       fakeUser = _createFakeUser(_vault, _userIndex);
-    } else {
-      fakeUser = FakeUser(computeAddress(_vault, _userIndex));
     }
-
+    Call[] memory calls = new Call[](1);
+    // calls[0] = Call({
+    //   to: _vault,
+    //   value: 0,
+    //   data: abi.encodeWithSelector(Vault.redeem.selector, balance, address(fakeUser), address(fakeUser)),
+    // });
+    fakeUser.executeCalls(calls);
   }
 
   /// @notice Computes the Loot Box address for a given Vault token.
@@ -60,7 +68,7 @@ contract UserFaker {
   /// @param _vault The vault
   /// @param _userIndex The user index
   /// @return The address of the Loot Box.
-  function computeAddress(address _vault, uint256 _userIndex) public view returns (address) {
+  function computeAddress(Vault _vault, uint256 _userIndex) public view returns (address) {
     return Create2.computeAddress(_salt(_vault, _userIndex), keccak256(fakeUserBytecode));
   }
 
@@ -68,7 +76,7 @@ contract UserFaker {
   /// @param _vault The Vault address
   /// @param _userIndex The user index
   /// @return The address of the newly created FakeUser.
-  function _createFakeUser(address _vault, uint256 _userIndex) internal returns (FakeUser) {
+  function _createFakeUser(Vault _vault, uint256 _userIndex) internal returns (FakeUser) {
     FakeUser fakeUser = FakeUser(Create2.deploy(0, _salt(_vault, _userIndex), fakeUserBytecode));
     fakeUser.initialize();
     return fakeUser;
@@ -78,8 +86,8 @@ contract UserFaker {
   /// @param _vault The Vault address
   /// @param _userIndex The user index
   /// @return A bytes32 value that is unique to that Vault token.
-  function _salt(address vault, uint256 _userIndex) internal pure returns (bytes32) {
-    return keccak256(abi.encodePacked(vault, _userIndex));
+  function _salt(Vault _vault, uint256 _userIndex) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(_vault, _userIndex));
   }
 
 }
